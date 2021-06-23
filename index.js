6640e78849523fc68bee4ee816ed2ab605a6bf5b
bannanaz <anlepet@gmail.com>
2021-06-23 14:08:56 +0200

    functions for: start game and play round

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ let level = 0;
 const startButton = document.querySelector(".js-start");
 const info = document.querySelector(".js-info");
 
+function activateTile(color) {
+  const tile = document.querySelector(`[data-tile="${color}"]`);
+  const sound = document.querySelector(`[data-sound="${color}"]`);
+
+  tile.classList.add("activated");
+  sound.play();
+
+  setTimeout(() => {
+    tile.classList.remove("activated");
+  }, 300);
+}
+
+function playRound(nextSequence) {
+  nextSequence.forEach((color, index) => {
+    setTimeout(() => {
+      activateTile(color);
+    }, (index + 1) * 600);
+  });
+}
+
 function nextStep() {
   const tiles = ["red", "beige", "green", "lilac"];
   const random = tiles[Math.floor(Math.random() * tiles.length)];
@@ -14,12 +34,15 @@ function nextStep() {
 function nextRound() {
   level += 1;
   const nextSequence = [...sequence];
+  nextSequence.push(nextStep());
+  playRound(nextSequence);
 }
 
 function startGame() {
   startButton.classList.add("hidden");
   info.classList.remove("hidden");
   info.textContent = "Wait for the computer";
+  nextRound();
 }
 
 startButton.addEventListener("click", startGame);
